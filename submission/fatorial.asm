fae427f5a3d4c90631613b69ec36623e8d13c88c
senderro <132297098+senderro@users.noreply.github.com>
2025-02-21 20:18:03 -0300

    teste 3

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,10 +1,9 @@
+@ /0000
+JP /0400
 
-@ /0100
-N   K =5 
+
+@ /0102
 RES K = 1
-
-
-@ /0200
 UM K = 1
 
 @ /0400  ; Início da sub-rotina
@@ -13,13 +12,13 @@ JZ FIM    ; Se AC == 0, pula para FIM (fatorial de 0 é 1)
 
 LOOP: 
     LD N      ; Carrega o valor de N para AC
+    JZ FIM    ; Se N == 0, termina
+    ML RES     ; Multiplica AC por RES
+    ST RES    ; Atualiza RES com o novo valor
+    LD N      ; Carrega o valor de N para AC
     SB UM     ; AC = AC - 1 (decrementa N)
     ST N      ; Atualiza N
-    JZ FIM    ; Se N == 0, termina
-    LD RES    ; Carrega RES em AC
-    ML N      ; Multiplica AC por N
-    ST RES    ; Atualiza RES com o novo valor
     JP LOOP   ; Volta para LOOP
 
 FIM:
-    HM        ; Halta a execução
+    HM /0000      ; 
