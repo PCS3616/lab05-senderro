d3e4f626bc6c4d0ec70f6b6a393ea344af4e2a80
senderro <132297098+senderro@users.noreply.github.com>
2025-02-21 23:35:41 -0300

    teste com o if

--- submission/io.asm
+++ submission/io.asm
@@ -1,5 +1,8 @@
 @ /000
-GD /000
+    JP START  ; Pula para o início do código
+
+@ /100
+START GD /000
 SB HEX_3030
 MM X_NUM
 
@@ -15,11 +18,22 @@ AD Y_NUM
 MM RESULT
 
 LD RESULT
+ML HEX_1000
+MM RESULT_TEMP
+
+LD HEX_A_1000
+SB RESULT_TEMP
+
+JN CARRY
+JP FINAL
+
+CARRY LD RESULT
 SB HEX_A;
 AD HEX_100;
 MM RESULT;
+JP FINAL;
 
-LD RESULT
+FINAL LD RESULT
 AD HEX_3030
 PD /100
 HM /0000
@@ -28,6 +42,12 @@ HM /0000
 X_NUM      K /0000  ; Primeiro dígito de X
 Y_NUM      K /0000  ; Primeiro dígito de Y
 RESULT     K /0000  ; Resultado da soma
+RESULT_TEMP K /0000 ; Valor temporario
+CONST_30   K /0030  ; Constante para conversão ASCII
+CONST_10   K /0010  ; Constante 10 (multiplicação)
 HEX_A      K /000A  ; Constante A para checar carry
+HEX_A_1000 K /A000
 HEX_100    K /0100  ; Constante para ajuste de carry
+HEX_1000    K /1000
+
 HEX_3030 K /3030
