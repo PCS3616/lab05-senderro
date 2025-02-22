b6d358ce8e22edc37838bb973d97c1c7bfef9185
senderro <132297098+senderro@users.noreply.github.com>
2025-02-21 21:26:08 -0300

    enderecamento relativo

--- submission/quadrados-perfeitos.asm
+++ submission/quadrados-perfeitos.asm
@@ -1,6 +1,5 @@
-@ /0400
+@ /0500
 ITER1 K =0      ; Contador externo (N)
-ITER2 K =1      ; Contador interno
 DOIS   K =2     ; Constante 2
 UM     K =1     ; Constante 1
 ZERO   K =0     ; Constante 0
@@ -11,33 +10,17 @@ SESQUATRO K =64  ; Limite (64 números, de 0 a 63)
 LOOP1 LD ITER1
 SB SESQUATRO
 JZ FIM       ; Se ITER1 == 64, termina
-
-LD UM
-MM ITER2     ; Reseta ITER2 para 1
-
-LD ZERO
-MM SOMA      ; Reseta SOMA para 0
-
-LOOP2 LD DOIS
-ML ITER2     ; Multiplica ITER2 por 2 (gera número ímpar 2i)
-AD UM        ; Soma 1 → 2i + 1
+ML ITER1     ; Multiplica ITER2 por 2
+AD UM        ;
 AD SOMA      ; Soma ao acumulador
 MM SOMA      ; Atualiza SOMA
-
-LD ITER2
-SB ITER1
 JZ SALVAR    ; Se ITER2 == ITER1, vai salvar
-AD UM
-MM ITER2
-JP LOOP2     ; Continua somando os ímpares
-
 SALVAR LD SOMA
-MM ENDER     ; Armazena o quadrado na memória
 
+MM &ENDER     ; Armazena o quadrado na memória
 LD ENDER
 AD DOIS      ; Próxima posição de memória
 MM ENDER
-
 LD ITER1
 AD UM        ; Incrementa ITER1 (próximo número)
 MM ITER1
